deb7c224804619b01eb5ef9693249bdcc28ee8d2
quan.le <quan.le@fii-na.com>
2022-10-04 15:30:17 -0500

    Add US Map to save_the_dates

--- wedding/urls.py
+++ wedding/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     # path(r'^$', views.Home.as_view(), name='home'),
     # path('home', views.Home.as_view(), name='home'),
     path('', views.Home.as_view(), name='home'),
-    path('home/hanoi', views.hanoi, name='hanoi'),
-    path('home/cantho', views.cantho, name='cantho'),
-    path('home/engagement', views.engagement, name='engagement'),
-    path('home/wedding', views.wedding, name='wedding-party'),
-    path('home/save-the-dates', views.save_the_date, name='save-the-dates-photos')
+    path('hanoi', views.hanoi, name='hanoi'),
+    path('cantho', views.cantho, name='cantho'),
+    path('engagement', views.engagement, name='engagement'),
+    path('wedding', views.wedding, name='wedding-party'),
+    path('save-the-dates', views.save_the_date, name='save-the-dates-photos')
 ]
